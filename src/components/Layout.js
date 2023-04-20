738d9b7000daabccaa8cfcd1a93cc0073e7646e0
dawnkelly09 <dawnkelly09@gmail.com>
2023-04-20 18:28:08 -0400

    sets custom font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,21 @@
-export default function AppLayout({ children }) {
+import { Inter } from 'next/font/google'
+
+const inter = Inter ({
+    subsets: ['latin'],
+    variable: "--font-inter"
+})
+
+const AppLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className={`${inter.variable} font-sans min-h-screen flex flex-col`}>
+        <header>header</header>
         <main className="flex-grow bg-[#f7f7f7]">
             { children }
         </main>
+        <footer>footer</footer>
 
     </div>
   )
 }
+
+export default AppLayout
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+      }
+    }
+  },
   plugins: [],
 }
